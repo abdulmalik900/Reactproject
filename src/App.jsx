78492f10a2899c82d999839e1e048da918cabcbe
abdulmalik900 <amalik90090@gmail.com>
2023-11-13 06:12:56 -0800

    i add something

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <Card
        
        img = "https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-       title = "UI/UX Review Check"
+       title = "Web designer Salman"
        des = " The place is close to Barceloneta Beach and bus stop just 2 min by walk
        and near to Naviglio where you can enjoy the main night life in
        Barcelona "
